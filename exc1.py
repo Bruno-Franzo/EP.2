10c720ae064e57a2501681dcebeb25c98f3471c4
Bruno <brunovf1@gmail.com>
2023-05-03 14:25:11 -0300

    Exercicio 1

---
+++ exc1.py
@@ -0,0 +1,29 @@
+def define_posicoes(l,c,ori,tam):
+    orientacao=ori
+    grid = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],/
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+  ]
+    posi0=[l,c]
+
+    posicoes=[]
+    posicoes.append(posi0)
+    if orientacao=='horizontal':
+        for i in range(1,tam):
+            c+=1
+            posi0=[l,c]
+            posicoes.append(posi0)
+    elif orientacao=='vertical':
+                for i in range(1,tam):
+                    l+=1
+                    posi0=[l,c]
+                    posicoes.append(posi0)
+    return posicoes
