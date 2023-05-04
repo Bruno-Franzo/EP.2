6c6aef3b179f32ebf396d7ff76ef41aacf4d9ad2
gus-trager <gustavto2@al.insper.edu.br>
2023-05-04 09:53:58 -0300

    Commit exc 2

---
+++ exc2.py
@@ -0,0 +1,26 @@
+def define_posicoes(l,c,ori,tam):
+    orientacao=ori
+    posi0=[l,c]
+
+    posicoes=[]
+    posicoes.append(posi0)
+    if orientacao=='horizontal':
+        for i in range(1,tam):
+            c+=1
+            posi0=[l,c]
+            posicoes.append(posi0)
+    elif orientacao=='vertical':
+                for i in range(1,tam):
+                    l+=1
+                    posi0=[l,c]
+                    posicoes.append(posi0)
+    return posicoes
+
+def preenche_frota(frota, navio, linha, coluna, orientacao, tamanho):
+    x = define_posicoes(linha,coluna,orientacao,tamanho)
+    if navio not in frota:
+        frota[navio] = [x]
+    else:
+        frota[navio].append(x)
+    
+    return frota
