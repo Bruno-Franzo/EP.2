ce7dc4822d718559d1a58b2cbecfc9ada2a765e9
Bruno <brunovf1@gmail.com>
2023-05-04 10:18:03 -0300

    exercicio 3

--- funções.py
+++ funções.py
@@ -26,3 +26,12 @@ def preenche_frota(frota, navio, linha, coluna, orientacao, tamanho):
         frota[navio].append(x)
     
     return frota
+
+#exercicio 3
+def faz_jogada(tabuleiro, linha,coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    elif tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    
+    return tabuleiro
